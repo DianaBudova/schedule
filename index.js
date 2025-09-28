d4d194e587fc30c09109e3352abb21dca4b9d46d
Diana <diana@Dianas-MacBook-Air.local>
2025-09-28 13:59:13 +0300

    fix code

--- index.js
+++ index.js
@@ -24,15 +24,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  console.log("REQ METHOD:", req.method);
-  console.log("REQ URL:", req.url);
-  console.log("REQ HEADERS:", req.headers);
-  console.log("REQ COOKIES:", req.cookies);   // якщо використовуєш cookie-parser
-  console.log("REQ BODY:", req.body);
-  console.log("REQ USER:", req.user);        // тут твоя авторизація через middleware
-  next();
-});
 
 // ---------- Supabase server client (service role) ----------
 const SUPABASE_URL = process.env.SUPABASE_URL;
@@ -174,6 +165,16 @@ function ensureAuth(req, res) {
 }
 
 function requireAuth(req, res, next) {
+  app.use((req, res, next) => {
+  console.log("REQ METHOD:", req.method);
+  console.log("REQ URL:", req.url);
+  console.log("REQ HEADERS:", req.headers);
+  console.log("REQ COOKIES:", req.cookies);   // якщо використовуєш cookie-parser
+  console.log("REQ BODY:", req.body);
+  console.log("REQ USER:", req.user);        // тут твоя авторизація через middleware
+  next();
+});
+
   if (req.user && req.user.id) return next();
 
   // Перевіряємо, чи очікується JSON (API) або звичайна сторінка
